9c853cbad0eea612bb5414a68b582bd6caa8a3e7
Ihor Stalevskiy <ihor.stalevskiy@artmyweb.com>
2021-05-06 20:07:57 +0300

    add product model

---
+++ lib/screen/products_overview_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProtuctsOverviewScreen extends StatelessWidget {
+  const ProtuctsOverviewScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('some protucts'),
+    );
+  }
+}
